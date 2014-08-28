a154538263ffc9d666fd01d803aff568a6a40aed
MukundRaghavSharma <MukundRaghavSharma@github.com>
2014-08-27 22:15:50 -0500

    restructured project

---
+++ src/Futures/Future.h
@@ -0,0 +1,17 @@
+#ifndef FUTURE_H
+#define FUTURE_H
+
+class Future
+{
+public:    
+    Future(double underlying, double rate, double timeToExpiration);
+    virtual ~Future();
+    virtual double Price();
+
+private:
+    double _underlying;
+    double _rate;
+    double _timeToExpiration;
+};
+
+#endif
---
+++ src/Options/Option.h
@@ -0,0 +1,8 @@
+#ifndef OPTION_H
+#define OPTION_H
+
+class Option
+{
+};
+
+#endif
